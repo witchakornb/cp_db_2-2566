fe79be82a465d2680f40bbee7ea9a03eb5deff56
Petch Thanathorn <petchthanathorn@gmail.com>
2024-02-23 18:00:31 +0700

    add 8

---
+++ web_database/app/product_import/create_product_import/page.js
@@ -0,0 +1,17 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Sell() {
+  return (
+    <>
+      <head>
+        <title>Forget Password</title>
+      </head>
+      <body>
+        <div className="container">
+          <h1>create_product_import</h1>
+        </div>
+      </body>
+    </>
+  );
+}
