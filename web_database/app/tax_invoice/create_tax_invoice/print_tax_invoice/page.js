1bb479fb06bc334078084b7f2460521c92ee8565
Petch Thanathorn <petchthanathorn@gmail.com>
2024-02-23 17:05:56 +0700

    add 7

---
+++ web_database/app/tax_invoice/create_tax_invoice/print_tax_invoice/page.js
@@ -0,0 +1,17 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Sell() {
+  return (
+    <>
+      <head>
+        <title>Forget Password</title>
+      </head>
+      <body>
+        <div className="container">
+          <h1>print_tax_invoice</h1>
+        </div>
+      </body>
+    </>
+  );
+}
