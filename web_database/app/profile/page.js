d2778c61353e3aecf11a94c8c5c910b57db8b6df
Petch Thanathorn <petchthanathorn@gmail.com>
2024-02-24 21:39:11 +0700

    PROFILE PAGE 2

--- web_database/app/profile/page.js
+++ web_database/app/profile/page.js
@@ -2,11 +2,12 @@ import Image from "next/image";
 import Link from "next/link";
 import styles from "./profile.css";
 //test by pond pond
+// import Document from "next/document";
 
 export default function Profile() {
    // let employeeId = document.getElementById('employee-id');
-   
-   
+   // employeeId.innerHTML = 'E0000000001'
+
    return (
       <>
          <head>
@@ -25,41 +26,43 @@ export default function Profile() {
                      </div>
                      <div class="profile-detail">
                         <div class="text-1">
-                           <label for="employee-id">รหัสพนักงาน</label><br/>
+                           <label for="employee-id">รหัสพนักงาน</label><br />
                            <input id="employee-id" type="text" name="employee-id" disabled />
                         </div>
                         <div class="text-2">
                            <div>
-                              <label for="first-name">ชื่อ</label><br/>
+                              <label for="first-name">ชื่อ</label><br />
                               <input id="first-name" type="text" name="first-name" disabled />
                            </div>
                            <div>
-                              <label for="last-name">นามสกุล</label><br/>
+                              <label for="last-name">นามสกุล</label><br />
                               <input id="last-name" type="text" name="last-name" disabled />
                            </div>
                         </div>
                         <div class="text-2">
                            <div>
-                              <label for="username">ชื่อผู้ใช้</label><br/>
+                              <label for="username">ชื่อผู้ใช้</label><br />
                               <input id="username" type="text" name="username" disabled />
                            </div>
                            <div>
-                              <label for="role">สิทธิ์</label><br/>
+                              <label for="role">สิทธิ์</label><br />
                               <input id="role" type="text" name="role" disabled />
                            </div>
                         </div>
                         <div class="text-2">
                            <div>
-                              <label for="email">อีเมล</label><br/>
+                              <label for="email">อีเมล</label><br />
                               <input id="email" type="text" name="email" disabled />
                            </div>
                            <div>
-                              <label for="phone-number">เบอร์โทร</label><br/>
+                              <label for="phone-number">เบอร์โทร</label><br />
                               <input id="phone-number" type="text" name="phone-number" disabled />
                            </div>
                         </div>
+                        <div className="btn">
+                           <a href="./sale">ตกลง</a>
+                        </div>
                      </div>
-                     <a href="./sale">ตกลง</a>
                   </div>
                </div>
 
