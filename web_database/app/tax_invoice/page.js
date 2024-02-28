cc91eec3a1c85197c88eb6f380685a95d3cf4aed
Phetchapa <phetchapa.n@kkumail.com>
2024-02-29 01:27:56 +0700

    tax and employee

--- web_database/app/tax_invoice/page.js
+++ web_database/app/tax_invoice/page.js
@@ -59,12 +59,25 @@ export default function tax_invoice() {
             </div>
           </div>
 
-          <div class="flex justify-between mx-4 mt-8">
-            <div class="font-bold">ข้อมูลพนักงาน</div>
+          <div class="font-bold mx-4 mt-8">ใบกำกับภาษีเต็มรูปแบบ</div>
+
+          <div class="flex justify-between mx-4 mt-4">
+            <div class="inline-block relative w-fit">
+              <select class="block appearance-none w-full rounded-md border border-[#e0e0e0] bg-white py-2 pl-6 pr-8 text-base outline-none focus:border-[#6A64F1] focus:shadow-md">
+                <option selected>ทั้งหมด</option>
+                <option>รอดำเนินการ</option>
+                <option>ดำเนินการเรียบร้อย</option>
+                <option>ข้อมูลถูกยกเลิก</option>
+              </select>
+              <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+                <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" /></svg>
+              </div>
+            </div>
+
             <div class="">
               <div class="ps-4">
                 <button class="bg-[#00A84F] hover:bg-[#008B41] text-white font-bold py-2 px-4 rounded inline-flex items-center">
-                  <span>เพิ่มพนักงานใหม่</span>
+                  <span>สร้างใบกำกับาษีเต็มรูปแบบ</span>
                 </button>
               </div>
             </div>
@@ -108,7 +121,7 @@ export default function tax_invoice() {
                             {isDropdown1Open && (
                               <div class="absolute mt-2 right-8 w-auto bg-white bg-gray-100 rounded-md shadow-xl z-10 ">
                                 <a href="#" class="block px-2 py-2 text-sm text-gray-300 text-gray-700 hover:bg-gray-400 hover:text-white">
-                                  <div class="inline-flex items-center">
+                                  <div class="flex items-start p-1">
                                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                                       <path stroke-linecap="round" stroke-linejoin="round" d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z" />
                                       <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
@@ -117,7 +130,7 @@ export default function tax_invoice() {
                                   </div>
                                 </a>
                                 <a href="#" class="block px-2 py-2 text-sm text-gray-300 text-gray-700 hover:bg-gray-400 hover:text-white">
-                                  <div class="inline-flex items-center">
+                                  <div class="flex items-start p-1">
                                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                                       <path stroke-linecap="round" stroke-linejoin="round" d="M6.72 13.829c-.24.03-.48.062-.72.096m.72-.096a42.415 42.415 0 0 1 10.56 0m-10.56 0L6.34 18m10.94-4.171c.24.03.48.062.72.096m-.72-.096L17.66 18m0 0 .229 2.523a1.125 1.125 0 0 1-1.12 1.227H7.231c-.662 0-1.18-.568-1.12-1.227L6.34 18m11.318 0h1.091A2.25 2.25 0 0 0 21 15.75V9.456c0-1.081-.768-2.015-1.837-2.175a48.055 48.055 0 0 0-1.913-.247M6.34 18H5.25A2.25 2.25 0 0 1 3 15.75V9.456c0-1.081.768-2.015 1.837-2.175a48.041 48.041 0 0 1 1.913-.247m10.5 0a48.536 48.536 0 0 0-10.5 0m10.5 0V3.375c0-.621-.504-1.125-1.125-1.125h-8.25c-.621 0-1.125.504-1.125 1.125v3.659M18 10.5h.008v.008H18V10.5Zm-3 0h.008v.008H15V10.5Z" />
                                     </svg>
