4656d7c246f3b3e8226a217139e3fc0ff3417574
Petch Thanathorn <petchthanathorn@gmail.com>
2024-03-03 01:27:28 +0700

    product_detail ver tailwind fn

--- web_database/app/sale/product_detail/page.js
+++ web_database/app/sale/product_detail/page.js
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import Link from "next/link";
 import styles from "./product_detail.css";
+import React from 'react';
 
 export default function ProductDetail() {
   async function getFertilizer(fertilizerId) {
@@ -20,71 +21,69 @@ export default function ProductDetail() {
       <head>
         <title>Product Detail</title>
       </head>
-      <body>
-        <div>
-          <div className="product-detail-box">
-            <div className="box-1">
-              <h3>รายละเอียดสินค้า</h3>
-              <a className="btn-close" href="/sale">&#10006;</a>
-            </div>
-            <div className="box-2">
-              <div className="chemicals">
-                <img src="/logo.jpg" alt="image" />
-                <div className="detail">
-                  <p>รหัสสินค้า : </p>
-                  <p>ชื่อสินค้า : </p>
-                  <p>หมวดหมู่ : </p>
-                  <p>ปริมาณ : </p>
-                  <p>ราคาขาย : </p>
-                  <p>จำนวนคงเหลือ : </p>
-                  <h4>รายละเอียดเพิ่มเติม</h4>
-                  <p>ชื่อสามัญของเคมีภัณฑ์ : </p>
-                  <p>ประเภทของเคมีภัณฑ์ : </p>
-                  <p>กลุ่มสารตาม IRAC : </p>
-                </div>
-              </div>
-              <div className="fertilizer">
-                <img src="/printer.png" alt="image" />
-                <div className="detail">
-                  <p>รหัสสินค้า : </p>
-                  <p>ชื่อสินค้า : </p>
-                  <p>หมวดหมู่ : </p>
-                  <p>ปริมาณ : </p>
-                  <p>ราคาขาย : </p>
-                  <p>จำนวนคงเหลือ : </p>
-                  <h4>รายละเอียดเพิ่มเติม</h4>
-                  <p>ชื่อสูตรปุ๋ย : </p>
-                  <p>ประเภทของปุ๋ย : </p>
-                </div>
-              </div>
-              <div className="craft_fertilizer">
-                <img src="/alert.png" alt="image" />
-                <div className="detail">
-                  <p>รหัสสินค้า : </p>
-                  <p>ชื่อสินค้า : </p>
-                  <p>หมวดหมู่ : </p>
-                  <p>ปริมาณ : </p>
-                  <p>ราคาขาย : </p>
-                  <p>จำนวนคงเหลือ : </p>
-                  <h4>รายละเอียดเพิ่มเติม</h4>
-                  <p>ชื่อสูตรปุ๋ย : </p>
-                  <p>ประเภทของปุ๋ย : </p>
-                </div>
-              </div>
-              <div className="Other">
-                <img src="/warning_red.png" alt="image" />
-                <div className="detail">
-                  <p>รหัสสินค้า : </p>
-                  <p>ชื่อสินค้า : </p>
-                  <p>ประเภทของสินค้า : </p>
-                  <p>ขนาดของสินค้า : </p>
-                  <p>ราคาขาย : </p>
-                  <p>หน่วยนับ : </p>
-                  <p>ปริมาณ / น้ำหนัก : </p>
-                  <p>จำนวนคงเหลือ : </p>
-                </div>
+      <body className="bg-red-500 text-center">
+        <div className="product-detail-box h-full w-96 relative shadow-lg text-center pt-16 pb-6 px-4 border border-gray-300 rounded-lg bg-white">
+          <div className="box-1 h-12 absolute top-0 left-0 right-0 text-start px-4 py-1 text-white bg-gray-600 rounded-t-lg flex justify-between items-center">
+            <h3>รายละเอียดสินค้า</h3>
+            <a className="btn-close text-white" href="/sale"><span className="text-white">&#10006;</span></a>
+          </div>
+          <div className="box-2 w-full px-2">
+            <div className="chemicals">
+              <img src="/logo.jpg" alt="image" className="w-44 mx-auto"/>
+              <div className="detail text-start mt-4">
+                <p>รหัสสินค้า : </p>
+                <p>ชื่อสินค้า : </p>
+                <p>หมวดหมู่ : </p>
+                <p>ปริมาณ : </p>
+                <p>ราคาขาย : </p>
+                <p>จำนวนคงเหลือ : </p>
+                <h4 className="font-bold mt-1 mb-1">รายละเอียดเพิ่มเติม</h4>
+                <p>ชื่อสามัญของเคมีภัณฑ์ : </p>
+                <p>ประเภทของเคมีภัณฑ์ : </p>
+                <p>กลุ่มสารตาม IRAC : </p>
               </div>
             </div>
+            {/* <div className="fertilizer">
+              <img src="/printer.png" alt="image" className="w-40" />
+              <div className="detail text-start px-4">
+                <p>รหัสสินค้า : </p>
+                <p>ชื่อสินค้า : </p>
+                <p>หมวดหมู่ : </p>
+                <p>ปริมาณ : </p>
+                <p>ราคาขาย : </p>
+                <p>จำนวนคงเหลือ : </p>
+                <h4 className="font-bold mt-1 mb-1">รายละเอียดเพิ่มเติม</h4>
+                <p>ชื่อสูตรปุ๋ย : </p>
+                <p>ประเภทของปุ๋ย : </p>
+              </div>
+            </div>
+            <div className="craft_fertilizer">
+              <img src="/alert.png" alt="image" className="w-40" />
+              <div className="detail text-start px-4">
+                <p>รหัสสินค้า : </p>
+                <p>ชื่อสินค้า : </p>
+                <p>หมวดหมู่ : </p>
+                <p>ปริมาณ : </p>
+                <p>ราคาขาย : </p>
+                <p>จำนวนคงเหลือ : </p>
+                <h4 className="font-bold mt-1 mb-1">รายละเอียดเพิ่มเติม</h4>
+                <p>ชื่อสูตรปุ๋ย : </p>
+                <p>ประเภทของปุ๋ย : </p>
+              </div>
+            </div>
+            <div className="Other">
+              <img src="/warning_red.png" alt="image" className="w-40" />
+              <div className="detail text-start px-4">
+                <p>รหัสสินค้า : </p>
+                <p>ชื่อสินค้า : </p>
+                <p>ประเภทของสินค้า : </p>
+                <p>ขนาดของสินค้า : </p>
+                <p>ราคาขาย : </p>
+                <p>หน่วยนับ : </p>
+                <p>ปริมาณ / น้ำหนัก : </p>
+                <p>จำนวนคงเหลือ : </p>
+              </div>
+            </div> */}
           </div>
         </div>
       </body>
