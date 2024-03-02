25087a6aa6bd2dcf4447ec16d994d8b42134a808
Petch Thanathorn <petchthanathorn@gmail.com>
2024-03-03 00:20:44 +0700

    product_detail 1

--- web_database/app/change_password/page.js
+++ web_database/app/change_password/page.js
@@ -9,26 +9,28 @@ export default function ChangePassword() {
         <title>Change password</title>
       </head>
       <body>
-      <div className="container">
-          <div class="change-password-box">
-            <div class="box-1">
-              <h3>เปลี่ยนรหัสผ่าน</h3>
-              <a className="btn-close" href="/sale">&#10006;</a>
-            </div>
-            <div className="box-2">
-              <div class="input-box">
-                <form action="#" method="post">
-                  <label for="old_password">รหัสผ่านเดิม</label><br />
-                  <input type="text" name="old_password" placeholder="กรอกรหัสผ่านเดิมของท่าน" /><br />
-                  <label for="new_password">รหัสผ่านใหม่</label><br />
-                  <input type="text" name="new_password" placeholder="กรอกรหัสผ่านใหม่" /><br />
-                  <label for="confirm_password">ยืนยันรหัสผ่านใหม่</label><br />
-                  <input type="text" name="confirm_password" placeholder="กรอกรหัสผ่านใหม่อีกครั้ง" /><br />
-                  <div className="btn-box">
-                    <button type="submit">ยืนยัน</button>
-                    <a href="/sale">ยกเลิก</a>
-                  </div>
-                </form>
+        <div>
+          <div className="container">
+            <div class="change-password-box">
+              <div class="box-1">
+                <h3>เปลี่ยนรหัสผ่าน</h3>
+                <a className="btn-close" href="/sale">&#10006;</a>
+              </div>
+              <div className="box-2">
+                <div class="input-box">
+                  <form action="#" method="post">
+                    <label for="old_password">รหัสผ่านเดิม</label><br />
+                    <input type="text" name="old_password" placeholder="กรอกรหัสผ่านเดิมของท่าน" /><br />
+                    <label for="new_password">รหัสผ่านใหม่</label><br />
+                    <input type="text" name="new_password" placeholder="กรอกรหัสผ่านใหม่" /><br />
+                    <label for="confirm_password">ยืนยันรหัสผ่านใหม่</label><br />
+                    <input type="text" name="confirm_password" placeholder="กรอกรหัสผ่านใหม่อีกครั้ง" /><br />
+                    <div className="btn-box">
+                      <button type="submit">ยืนยัน</button>
+                      <a href="/sale">ยกเลิก</a>
+                    </div>
+                  </form>
+                </div>
               </div>
             </div>
           </div>
--- web_database/app/sale/product_detail/page.js
+++ web_database/app/sale/product_detail/page.js
@@ -1,15 +1,91 @@
 import Image from "next/image";
 import Link from "next/link";
+import styles from "./product_detail.css";
 
-export default function Sell() {
+export default function ProductDetail() {
+  async function getFertilizer(fertilizerId) {
+    
+    const response = await axios.post(`${process.env.NEXT_PUBLIC_IP}/insert_customer`,
+    {
+      Item_ItemId: fertilizerId,
+    },
+    {
+      withCredentials: true,
+    });
+    console.log(response.data);
+    return response.data;
+  }
   return (
     <>
       <head>
         <title>Product Detail</title>
       </head>
       <body>
-        <div className="container">
-          <h1>product_detail</h1>
+        <div>
+          <div className="product-detail-box">
+            <div className="box-1">
+              <h3>รายละเอียดสินค้า</h3>
+              <a className="btn-close" href="/sale">&#10006;</a>
+            </div>
+            <div className="box-2">
+              <div className="chemicals">
+                <img src="/logo.jpg" alt="image" />
+                <div className="detail">
+                  <p>รหัสสินค้า : </p>
+                  <p>ชื่อสินค้า : </p>
+                  <p>หมวดหมู่ : </p>
+                  <p>ปริมาณ : </p>
+                  <p>ราคาขาย : </p>
+                  <p>จำนวนคงเหลือ : </p>
+                  <h4>รายละเอียดเพิ่มเติม</h4>
+                  <p>ชื่อสามัญของเคมีภัณฑ์ : </p>
+                  <p>ประเภทของเคมีภัณฑ์ : </p>
+                  <p>กลุ่มสารตาม IRAC : </p>
+                </div>
+              </div>
+              <div className="fertilizer">
+                <img src="/printer.png" alt="image" />
+                <div className="detail">
+                  <p>รหัสสินค้า : </p>
+                  <p>ชื่อสินค้า : </p>
+                  <p>หมวดหมู่ : </p>
+                  <p>ปริมาณ : </p>
+                  <p>ราคาขาย : </p>
+                  <p>จำนวนคงเหลือ : </p>
+                  <h4>รายละเอียดเพิ่มเติม</h4>
+                  <p>ชื่อสูตรปุ๋ย : </p>
+                  <p>ประเภทของปุ๋ย : </p>
+                </div>
+              </div>
+              <div className="craft_fertilizer">
+                <img src="/alert.png" alt="image" />
+                <div className="detail">
+                  <p>รหัสสินค้า : </p>
+                  <p>ชื่อสินค้า : </p>
+                  <p>หมวดหมู่ : </p>
+                  <p>ปริมาณ : </p>
+                  <p>ราคาขาย : </p>
+                  <p>จำนวนคงเหลือ : </p>
+                  <h4>รายละเอียดเพิ่มเติม</h4>
+                  <p>ชื่อสูตรปุ๋ย : </p>
+                  <p>ประเภทของปุ๋ย : </p>
+                </div>
+              </div>
+              <div className="Other">
+                <img src="/warning_red.png" alt="image" />
+                <div className="detail">
+                  <p>รหัสสินค้า : </p>
+                  <p>ชื่อสินค้า : </p>
+                  <p>ประเภทของสินค้า : </p>
+                  <p>ขนาดของสินค้า : </p>
+                  <p>ราคาขาย : </p>
+                  <p>หน่วยนับ : </p>
+                  <p>ปริมาณ / น้ำหนัก : </p>
+                  <p>จำนวนคงเหลือ : </p>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </body>
     </>
