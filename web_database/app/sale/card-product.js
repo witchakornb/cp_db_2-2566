aa37f4793c76a6eb4c26d6c883c2c0d49715ae25
PattheeraTee <pattheera.mwny@gmail.com>
2024-03-04 09:04:50 +0700

    add sale with product and product detail

--- web_database/app/sale/card-product.js
+++ web_database/app/sale/card-product.js
@@ -1,12 +1,26 @@
+'use cleint';
+import React,{useState} from 'react';
 
-import React from 'react';
-
-const ProductCard = ({ product }) => {
+const ProductCard = ({ product , onViewDetails}) => {
   // Provide default values if 'product' is undefined
-  const { fertilizerName = '', fertilizerPrice = 0, ItemPhoto: fertilizerItemPhoto = '' } = product || {};
-const { ChemicalName = '', ChemicalPrice = 0, ItemPhoto: ChemicalItemPhoto = '' } =  product || {};
-const { OtherName = '', OtherPrice = 0, ItemPhoto: OtherItemPhoto = '' } = product || {};
-const { Craft_fertilizerName = '', Craft_fertilizerPrice = 0, ItemPhoto: CraftItemPhoto = '' } = product || {};
+  const { Item_ItemId: fertilizerItemId = '' ,fertilizerName = '', fertilizerPrice = 0, ItemPhoto: fertilizerItemPhoto = '' } = product || {};
+const { Item_ItemId: ChemicalItemId = '' ,ChemicalName = '', ChemicalPrice = 0, ItemPhoto: ChemicalItemPhoto = '' } =  product || {};
+const { Item_ItemId: OtherItemId = '' ,OtherName = '', OtherPrice = 0, ItemPhoto: OtherItemPhoto = '' } = product || {};
+const { Item_ItemId: Craft_fertilizerItemId = '' ,Craft_fertilizerName = '', Craft_fertilizerPrice = 0, ItemPhoto: CraftItemPhoto = '' } = product || {};
+const [isDropdownOpen, setDropdownOpen] = useState(false);
+
+
+  const toggleDropdown = (e) => {
+    e.preventDefault(); // Prevent default behavior
+    setDropdownOpen(!isDropdownOpen);
+  };
+const handleViewDetails = (productId) => {
+  productId = fertilizerItemId||ChemicalItemId||OtherItemId||Craft_fertilizerItemId;
+  console.log('use client in product card product Id:', productId);
+  onViewDetails(productId);
+  setDropdownOpen(false);
+
+};
 
   return (
     <div className="w-1/5 bg-white rounded-lg shadow border border-black m-2">
@@ -18,20 +32,41 @@ const { Craft_fertilizerName = '', Craft_fertilizerPrice = 0, ItemPhoto: CraftIt
       <div className="p-4 bg-gray-900 rounded-b">
         <a href="#" className="flex justify-between">
           <h5 className="mb-2 text-xl font-bold tracking-tight text-white">{fertilizerName||ChemicalName||OtherName||Craft_fertilizerName}</h5>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke-width="1.5"
-            stroke="currentColor"
-            className="w-6 h-6 text-white"
+          <div className="relative">
+      <button
+        onClick={toggleDropdown}
+        className="p-2 bg-gray-900 rounded-md"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth="1.5"
+          stroke="currentColor"
+          className="w-6 h-6 text-white"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M12 6.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5ZM12 12.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5ZM12 18.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5Z"
+          />
+        </svg>
+      </button>
+
+      {isDropdownOpen && (
+        <div className="absolute mt-2 right-0 w-40 bg-white bg-gray-100 rounded-md shadow-xl z-10">
+          <button
+            className="w-full block px-2 py-2 text-sm text-gray-300 text-gray-700 hover:bg-gray-400 hover:text-white"
+            onClick={handleViewDetails}
           >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              d="M12 6.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5ZM12 12.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5ZM12 18.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5Z"
-            />
-          </svg>
+            <div className="flex items-start p-1">
+              <span className="mr-1">🔍</span>
+              <span>ดูรายละเอียดสินค้า</span>
+            </div>
+          </button>
+        </div>
+      )}
+    </div>
         </a>
         <p className="mb-3 font-normal text-white">{fertilizerPrice||Craft_fertilizerPrice||OtherPrice||ChemicalPrice} บาท</p>
       </div>
--- web_database/app/sale/product_detail/page.js
+++ web_database/app/sale/product_detail/page.js
@@ -1,92 +1,140 @@
+'use client';
 import Image from "next/image";
 import Link from "next/link";
 import styles from "./product_detail.css";
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-export default function ProductDetail() {
-  async function getFertilizer(fertilizerId) {
-    
-    const response = await axios.post(`${process.env.NEXT_PUBLIC_IP}/insert_customer`,
-    {
-      Item_ItemId: fertilizerId,
-    },
-    {
-      withCredentials: true,
-    });
-    console.log(response.data);
-    return response.data;
-  }
+
+export default function ProductDetail({ itemId }) {
+  const [fertilizer, setfertilizer] = useState(null);
+  const [craft_fertilizer, setcraft_fertilizer] = useState(null);
+  const [chemical, setchemical] = useState(null);
+  const [other, setother] = useState(null);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        console.log('itemId: ', itemId); // แสดงค่า itemId ที่ส่งเข้าไปใน API
+        // เรียกใช้งาน API แต่ละตัวโดยไม่รอผลลัพธ์ก่อนส่งไปยังตัวต่อไป
+        axios.post(`${process.env.NEXT_PUBLIC_IP}/user/select_chemicalsById`, { Item_ItemId: itemId }, { withCredentials: true })
+          .then(responseChemical => {
+            console.log('Chemicals data : ', responseChemical.data);
+            if (responseChemical.data) {
+              setchemical(responseChemical.data);
+            }
+          })
+          .catch(error => console.error('Error fetching chemicals data:', error));
+  
+        axios.post(`${process.env.NEXT_PUBLIC_IP}/user/select_fertilizerById`, { Item_ItemId: itemId }, { withCredentials: true })
+          .then(responseFertilizer => {
+            console.log('Fertilizer data : ', responseFertilizer.data);
+            if (responseFertilizer.data) {
+              setfertilizer(responseFertilizer.data);
+            }
+          })
+          .catch(error => console.error('Error fetching fertilizer data:', error));
+  
+        axios.post(`${process.env.NEXT_PUBLIC_IP}/user/select_craftById`, { Item_ItemId: itemId }, { withCredentials: true })
+          .then(responseCraft => {
+            console.log('Craft data : ', responseCraft.data);
+            if (responseCraft.data) {
+              setcraft_fertilizer(responseCraft.data);
+            }
+          })
+          .catch(error => console.error('Error fetching craft data:', error));
+  
+        axios.post(`${process.env.NEXT_PUBLIC_IP}/user/select_otherById`, { Item_ItemId: itemId }, { withCredentials: true })
+          .then(responseOther => {
+            console.log('Other data : ', responseOther.data);
+            if (responseOther.data) {
+              setother(responseOther.data);
+            }
+          })
+          .catch(error => console.error('Error fetching other data:', error));
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+  
+    fetchData();
+  }, [itemId]);
+  
   return (
-    <>
-      <head>
-        <title>Product Detail</title>
-      </head>
-      <body className="bg-red-500 text-center">
-        <div className="product-detail-box h-full w-96 relative shadow-lg text-center pt-16 pb-6 px-4 border border-gray-300 rounded-lg bg-white">
-          <div className="box-1 h-12 absolute top-0 left-0 right-0 text-start px-4 py-1 text-white bg-gray-600 rounded-t-lg flex justify-between items-center">
-            <h3>รายละเอียดสินค้า</h3>
-            <a className="btn-close text-white" href="/sale"><span className="text-white">&#10006;</span></a>
-          </div>
-          <div className="box-2 w-full px-4">
-            <div className="chemicals">
-              <img src="/logo.jpg" alt="image" className="w-44 mx-auto"/>
-              <div className="detail text-start mt-4">
-                <p>รหัสสินค้า : </p>
-                <p>ชื่อสินค้า : </p>
-                <p>หมวดหมู่ : </p>
-                <p>ปริมาณ : </p>
-                <p>ราคาขาย : </p>
-                <p>จำนวนคงเหลือ : </p>
-                <h4 className="font-bold mt-1 mb-1">รายละเอียดเพิ่มเติม</h4>
-                <p>ชื่อสามัญของเคมีภัณฑ์ : </p>
-                <p>ประเภทของเคมีภัณฑ์ : </p>
-                <p>กลุ่มสารตาม IRAC : </p>
-              </div>
-            </div>
-            {/* <div className="fertilizer">
-              <img src="/printer.png" alt="image" className="w-40" />
-              <div className="detail text-start px-4">
-                <p>รหัสสินค้า : </p>
-                <p>ชื่อสินค้า : </p>
-                <p>หมวดหมู่ : </p>
-                <p>ปริมาณ : </p>
-                <p>ราคาขาย : </p>
-                <p>จำนวนคงเหลือ : </p>
-                <h4 className="font-bold mt-1 mb-1">รายละเอียดเพิ่มเติม</h4>
-                <p>ชื่อสูตรปุ๋ย : </p>
-                <p>ประเภทของปุ๋ย : </p>
-              </div>
-            </div>
-            <div className="craft_fertilizer">
-              <img src="/alert.png" alt="image" className="w-40" />
-              <div className="detail text-start px-4">
-                <p>รหัสสินค้า : </p>
-                <p>ชื่อสินค้า : </p>
-                <p>หมวดหมู่ : </p>
-                <p>ปริมาณ : </p>
-                <p>ราคาขาย : </p>
-                <p>จำนวนคงเหลือ : </p>
-                <h4 className="font-bold mt-1 mb-1">รายละเอียดเพิ่มเติม</h4>
-                <p>ชื่อสูตรปุ๋ย : </p>
-                <p>ประเภทของปุ๋ย : </p>
-              </div>
-            </div>
-            <div className="Other">
-              <img src="/warning_red.png" alt="image" className="w-40" />
-              <div className="detail text-start px-4">
-                <p>รหัสสินค้า : </p>
-                <p>ชื่อสินค้า : </p>
-                <p>ประเภทของสินค้า : </p>
-                <p>ขนาดของสินค้า : </p>
-                <p>ราคาขาย : </p>
-                <p>หน่วยนับ : </p>
-                <p>ปริมาณ / น้ำหนัก : </p>
-                <p>จำนวนคงเหลือ : </p>
-              </div>
-            </div> */}
-          </div>
+    <div className="absolute inset-0 bg-black bg-opacity-30 backdrop-blur-sm flex justify-center items-center h-full ">
+      <div className="chemical-detail-box h-fit w-96 relative shadow-lg text-center pt-16 pb-6 px-4 border border-gray-300 rounded-lg bg-white">
+        <div className="box-1 h-12 absolute top-0 left-0 right-0 text-start px-4 py-1 text-white bg-gray-600 rounded-t-lg flex justify-between items-center">
+          <h3>รายละเอียดสินค้า</h3>
+          <a className="text-white" href="/sale">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
+            </svg>
+
+          </a>
         </div>
-      </body>
-    </>
+        <div className="detail text-start mt-4">
+          {chemical && (
+            <>
+              <img src={`data:image/jpeg;base64,${chemical.ItemPhoto}`} alt="image" className="w-44 mx-auto mb-4" />
+              <p className="mb-2">รหัสสินค้า: {chemical.Item_ItemId}</p>
+              <p className="mb-2">ชื่อสินค้า: {chemical.ChemicalName}</p>
+              <p className="mb-2">หมวดหมู่: {chemical.ItemType}</p>
+              <p className="mb-2">ปริมาณ: {chemical.Chemicalweight} {chemical.ChemicalUnitName}</p>
+              <p className="mb-2">ราคาขาย: {chemical.ChemicalPrice} บาท</p>
+              <p className="mb-2">จำนวนคงเหลือ: {chemical.ItemAmount} {chemical.ItemUnitName}</p>
+              <h4 className="font-bold mt-3 mb-3">รายละเอียดเพิ่มเติม</h4>
+              <p className="mb-2">ชื่อสามัญของเคมีภัณฑ์ : {chemical.ChemicalCommonName}</p>
+              <p className="mb-2">ประเภทของเคมีภัณฑ์ : {chemical.ChemicalType}</p>
+              <p className="mb-0">กลุ่มสารตาม IRAC : {chemical.ChemicalIRAC}</p>
+            </>
+          )}
+        </div>
+        <div className="detail text-start mt-4">
+          {fertilizer && (
+            <>
+              <img src={`data:image/jpeg;base64,${fertilizer.ItemPhoto}`} alt="image" className="w-44 mx-auto mb-4" />
+              <p className="mb-2">รหัสสินค้า: {fertilizer.Item_ItemId}</p>
+              <p className="mb-2">ชื่อสินค้า: {fertilizer.FertilizerName}</p>
+              <p className="mb-2">หมวดหมู่: {fertilizer.ItemType}</p>
+              <p className="mb-2">ปริมาณ: {fertilizer.FertilizerWeigth} {fertilizer.FertilizerUnitName}</p>
+              <p className="mb-2">ราคาขาย: {fertilizer.FertilizerPrice} บาท</p>
+              <p className="mb-2">จำนวนคงเหลือ: {fertilizer.ItemAmount} {fertilizer.ItemUnitName}</p>
+              <h4 className="font-bold mt-3 mb-3">รายละเอียดเพิ่มเติม</h4>
+              <p className="mb-2">ชื่อสูตรปุ๋ย : {fertilizer.FertilizerFormulaName}</p>
+              <p className="">ประเภทของปุ๋ย : {fertilizer.FertilizerType}</p>
+            </>
+          )}
+        </div>
+        <div className="detail text-start mt-4">
+          {craft_fertilizer && (
+            <>
+              <img src={`data:image/jpeg;base64,${craft_fertilizer.ItemPhoto}`} alt="image" className="w-44 mx-auto mb-4" />
+              <p className="mb-2">รหัสสินค้า: {craft_fertilizer.Item_ItemId}</p>
+              <p className="mb-2">ชื่อสินค้า: {craft_fertilizer.CraftName}</p>
+              <p className="mb-2">หมวดหมู่: {craft_fertilizer.ItemType}</p>
+              <p className="mb-2">ปริมาณ: {craft_fertilizer.CraftWeigth} {craft_fertilizer.CraftUnitName}</p>
+              <p className="mb-2">ราคาขาย: {craft_fertilizer.CraftPrice} บาท</p>
+              <p className="mb-2">จำนวนคงเหลือ: {craft_fertilizer.ItemAmount} {craft_fertilizer.ItemUnitName}</p>
+              <h4 className="font-bold mt-3 mb-3">รายละเอียดเพิ่มเติม</h4>
+              <p className="mb-2">ชื่อสูตรปุ๋ย : {craft_fertilizer.CraftFormulaName}</p>
+              <p className="">ประเภทของปุ๋ย : {craft_fertilizer.CraftType}</p>
+            </>
+          )}
+        </div>
+        <div className="detail text-start mt-4">
+          {other && (
+            <>
+              <img src={`data:image/jpeg;base64,${other.ItemPhoto}`} alt="image" className="w-44 mx-auto mb-4" />
+              <p className="mb-2">รหัสสินค้า: {other.Item_ItemId}</p>
+              <p className="mb-2">ชื่อสินค้า: {other.OtherlName}</p>
+              <p className="mb-2">ประเภทของสินค้า : {other.ItemType}</p>
+              <p className="mb-2">ขนาดของสินค้า : {other.OtherSize}</p>
+              <p className="mb-2">ราคาขาย : {other.OtherPrice} บาท</p>
+              <p className="mb-2">ปริมาณ / น้ำหนัก :  {other.OtherWeigth} {other.OtherUnitName}</p>
+              <p className="mb-0">จำนวนคงเหลือ: {other.ItemAmount} {other.ItemUnitName}</p>
+            </>
+          )}
+        </div>
+      </div>
+    </div>
   );
 }
