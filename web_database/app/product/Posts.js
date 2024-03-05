aa3a7ee47b6f16e39c3af1c3ea7493658f7b7f76
Phetchapa <phetchapa.n@kkumail.com>
2024-03-05 15:41:24 +0700

    sweet alert

--- web_database/app/product/Posts.js
+++ web_database/app/product/Posts.js
@@ -4,6 +4,7 @@ import Link from "next/link";
 import styles from "./product.css";
 import axios from 'axios';
 import { useState, useEffect } from "react";
+import Swal from 'sweetalert2/dist/sweetalert2.js'
 
 const Posts = ({ posts }) => {
 
@@ -90,7 +91,25 @@ const Posts = ({ posts }) => {
                             </td>
                             <td class="whitespace-nowrap  px-6 py-4">
                                 <div class="">
-                                    <button onClick={() => handleDelete(post)}
+                                    <button onClick={() => Swal.fire({
+                                        title: "Are you sure?",
+                                        text: "You won't be able to revert this!" + post.Item_ItemId,
+                                        icon: "warning",
+                                        showCancelButton: true,
+                                        confirmButtonColor: "#3085d6",
+                                        cancelButtonColor: "#d33",
+                                        confirmButtonText: "Yes, delete it!"
+                                    }).then((result) => {
+                                        if (result.isConfirmed) {
+                                            Swal.fire({
+                                                title: "Deleted!",
+                                                text: "Your file has been deleted.",
+                                                icon: "success"
+                                            }
+                                            ),
+                                                handleDelete(post);
+                                        }
+                                    })}
                                         class="bg-[#FA0000] hover:bg-[#008B41] text-white font-bold p-1 rounded inline-flex items-center">
                                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                                             <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
