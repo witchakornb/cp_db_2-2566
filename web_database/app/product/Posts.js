c389a643b3e4cef5eb89d5349805a1ad661c2187
Phetchapa <phetchapa.n@kkumail.com>
2024-03-05 12:27:54 +0700

    product

---
+++ web_database/app/product/Posts.js
@@ -0,0 +1,106 @@
+'use client';
+import Image from "next/image";
+import Link from "next/link";
+import styles from "./product.css";
+import axios from 'axios';
+import { useState, useEffect } from "react";
+
+const Posts = ({ posts }) => {
+
+    const [allPosts, setPosts] = useState([]);
+
+    useEffect(() => {
+        const fetchPosts = async () => {
+            try {
+                const response = await axios.get(`${process.env.NEXT_PUBLIC_IP}/user/show_all_item_big`);
+                const { Fertilizer, Chemicals, Other, Craft } = response.data;
+                const fertilizers = Fertilizer || [];
+                const chemicals = Chemicals || [];
+                const others = Other || [];
+                const crafts = Craft || [];
+                const allData = [...fertilizers, ...chemicals, ...others, ...crafts];
+                console.log(allData);
+                setPosts(allData);
+            } catch (error) {
+                console.error('Error fetching posts:', error);
+            }
+        };
+        fetchPosts();
+    }, []);
+
+    console.log("Posts : ", posts);
+
+    const handleDelete = async (post) => {
+        try {
+            setPosts(posts.filter((p) => p.Item_ItemId != post.Item_ItemId))
+            console.log("Item_ItemId : ", post.Item_ItemId);
+            const response = await axios.post(`${process.env.NEXT_PUBLIC_IP}/show_all_item_big/remove`, post.Item_ItemId,
+                {
+                    withCredentials: true,
+                }
+            );
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    return (
+        <div className="">
+            <table class="min-w-full text-center text-sm font-light">
+                <thead
+                    class="border-b bg-[#777777] font-medium text-white">
+                    <tr>
+                        <th scope="col" class=" px-6 py-4">#</th>
+                        <th scope="col" class=" px-6 py-4">รหัสสินค้า</th>
+                        <th scope="col" class=" px-6 py-4">ชื่อสินค้า</th>
+                        <th scope="col" class=" px-6 py-4">ประเภท</th>
+                        <th scope="col" class=" px-6 py-4">ราคาที่ขาย</th>
+                        <th scope="col" class=" px-6 py-4">จำนวน</th>
+                        <th scope="col" class=" px-6 py-4">หน่วย</th>
+                        <th scope="col" class=" px-6 py-4">แก้ไข</th>
+                        <th scope="col" class=" px-6 py-4">ลบ</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {posts.map((post, index) => (
+                        <tr key={post.Item_ItemId}
+                            class="border-b dark:border-neutral-500">
+                            <td class="whitespace-nowrap  px-6 py-4">{index + 1}</td>
+                            <td class="whitespace-nowrap  px-6 py-4">{post.Item_ItemId}</td>
+                            <td class="whitespace-nowrap  px-6 py-4">{post.fertilizerName || post.ChemicalName || post.OtherName || post.Craft_fertilizerName}</td>
+                            <td class="whitespace-nowrap  px-6 py-4">{post.ItemType}</td>
+                            <td class="whitespace-nowrap  px-6 py-4">{post.fertilizerPrice || post.ChemicalPrice || post.OtherPrice || post.Craft_fertilizerPrice}</td>
+                            <td class="whitespace-nowrap  px-6 py-4">{post.ItemAmount}</td>
+                            <td class="whitespace-nowrap  px-6 py-4">{post.UnitName}</td>
+                            <td class="whitespace-nowrap  px-6 py-4">
+                                <div class="">
+                                    <Link href={`/edit_customer/${post.Item_ItemId}`}>
+                                        <button
+                                            class="bg-[#777777] hover:bg-[#008B41] text-white font-bold p-1 rounded inline-flex items-center">
+                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                                <path stroke-linecap="round" stroke-linejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
+                                            </svg>
+                                        </button>
+                                    </Link>
+                                </div>
+                            </td>
+                            <td class="whitespace-nowrap  px-6 py-4">
+                                <div class="">
+                                    <button onClick={() => handleDelete(post)}
+                                        class="bg-[#FA0000] hover:bg-[#008B41] text-white font-bold p-1 rounded inline-flex items-center">
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                            <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+                                        </svg>
+                                    </button>
+                                </div>
+                            </td>
+                        </tr>
+                    ))}
+
+                </tbody>
+            </table>
+        </div>
+    );
+};
+
+export default Posts
--- web_database/app/product/page.js
+++ web_database/app/product/page.js
@@ -1,174 +1,275 @@
-'use client'
-import { useState, useEffect } from "react";
+'use client';
+import Image from "next/image";
+import Link from "next/link";
+import styles from "./product.css";
+import Posts from "./Posts";
 import axios from 'axios';
+import { useState, useEffect } from "react";
 
-export default function Page() {
-  const [fertilizerUnitId, setfertilizerUnitId] = useState([]);
-  const [ItemUnitId, setItemUnitId] = useState([]);
-  const [ItemId, setItemId] = useState("");
+export default function Customer() {
 
-  const [selectedFile, setSelectedFile] = useState(null);
-  const [base64String, setBase64String] = useState('');
-  const [image, setImage] = useState("");
+  // get Data Api
+  const [customerIdOptions, setcustomerIdOptions] = useState([]);
+  const [customerNameOptions, setcustomerNameOptions] = useState([]);
+  const [posts, setPosts] = useState([]);
 
-  async function onSubmit(event) {
-    if (!base64String) {
-      alert('Please select an image first.')
-      return
-    }
-
-    event.preventDefault();
-    const form = event.target;
-
-    const output = {
-      Item_ItemId: form.Item_ItemId.value,
-      FertilizerName: form.FertilizerName.value,
-      FertilizerFormulaName: form.FertilizerFormulaName.value,
-      FertilizerType: form.FertilizerType.value,
-      FertilizerPrice: parseFloat(form.FertilizerPrice.value),
-      FertilizerUnitId: parseInt(form.FertilizerUnitId.value),
-      ItemUnitId: parseInt(form.ItemUnitId.value),
-      FertilizerWeigth: parseFloat(form.FertilizerWeigth.value),
-      FertilizerPhoto: base64String,
-    };
-    try {
-      const response = await axios.post(
-        'http://10.48.104.125:8080/user/insert_fertilizer',
-        output,
-        {
-          withCredentials: true,
-        }
-      );
-    } catch (error) {
-      console.error('Error:', error);
-    }
-  }
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const ItemId = await axios.get(
-          'http://10.48.104.125:8080/user/get_item_name_for_fertilizer',
-          {
-            withCredentials: true,
-          }
-        );
-        setItemId(ItemId.data.ItemId);
-        fertilizerUnitId
-      } catch (error) {
-        console.error('Error:', error);
-      }
-      // -------------------
-      try {
-        const response = await axios.get(
-          'http://10.48.104.125:8080/user/get_unit_for_item',
-          {
-            withCredentials: true,
-          }
-        );
-        setItemUnitId(response.data);
-        fertilizerUnitId
-      } catch (error) {
-        console.error('Error:', error);
-      }
-      // -------------------
-      try {
-        const response = await axios.get(
-          'http://10.48.104.125:8080/user/get_unit_for_product',
-          {
-            withCredentials: true,
-          }
-        );
-        setfertilizerUnitId(response.data);
-        fertilizerUnitId
-      } catch (error) {
-        console.error('Error:', error);
-      }
+        const response = await axios.get(`${process.env.NEXT_PUBLIC_IP}/user/show_all_item_big`);
+        const { Fertilizer, Chemicals, Other, Craft } = response.data;
+        const fertilizers = Fertilizer || [];
+        const chemicals = Chemicals || [];
+        const others = Other || [];
+        const crafts = Craft || [];
+        const allData = [...fertilizers, ...chemicals, ...others, ...crafts];
+        console.log(allData);
+        setPosts(allData);
 
-    }
+        // Set options for search
+        const optionsCustomerId = allData.map((item) => (
+          <option key={item.Item_ItemId} value={item.Item_ItemId}></option>
+        ));
+        setcustomerIdOptions(optionsCustomerId);
+        console.log('options customerId :', optionsCustomerId);
+
+        // Helper function to get the appropriate display name based on ItemType
+        const getDisplayName = (item) => {
+          switch (item.ItemType) {
+            case "Fertilizer":
+              return item.fertilizerName;
+            case "Chemicals":
+              return item.ChemicalName;
+            case "Other":
+              return item.OtherName;
+            case "Craft":
+              return item.Craft_fertilizerName;
+            default:
+              return "";
+          }
+        };
+
+        const optionsCustomerName = allData.map((item) => (
+          <option key={item.Item_ItemId} value={getDisplayName(item)}></option>
+        ));
+        setcustomerNameOptions(optionsCustomerName);
+        console.log('options customerName :', optionsCustomerName);
+
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
     fetchData();
   }, []);
 
-  const handleFileChange = (event) => {
-    const imageInput = event.target;
-    const image = imageInput.files[0];
-    setImage(image);
-    if (!image) {
-      console.error('No image file selected.');
-      return;
-    }
-    const reader = new FileReader();
-    reader.onload = () => {
-      const result = reader.result;
-      // Validate data URL format and extract base64 string
-      if (result.startsWith('data:image/png;base64,')) {
-        setBase64String(result.replace('data:image/png;base64,', ''));
-      } else if (result.startsWith('data:image/jpeg;base64,')) {
-        setBase64String(result.replace('data:image/jpeg;base64,', ''));
-      } else {
-        console.error('Unsupported image format. Please select a PNG or JPEG.');
-        return;
-      }
+  //search
+  const [searchButtonClicked, setSearchButtonClicked] = useState(false);
+  const [filteredSalesData, setFilteredSalesData] = useState([]);
+  const [InputcustomerID, setInputcustomerID] = useState('');
+  const [InputcustomerName, setInputcustomerName] = useState('');
+  const filterSalesData = () => {
+    if (searchButtonClicked) {
+      console.log('customerID input:', InputcustomerID);
+      console.log('customerName input:', InputcustomerName);
 
-      setSelectedFile(image);
-    };
-    reader.onerror = (error) => {
-      console.error('Error reading file:', error);
-    };
-    reader.readAsDataURL(image);
+      const filteredData = posts.filter((sale) => {
+        const inputCustomerIDMatch = !InputcustomerID || sale.Item_ItemId.includes(InputcustomerID);
+        const inputCustomerNameMatch =
+          !InputcustomerName ||
+          ((sale.fertilizerName && sale.fertilizerName.includes(InputcustomerName)) ||
+            (sale.ChemicalName && sale.ChemicalName.includes(InputcustomerName)) ||
+            (sale.OtherName && sale.OtherName.includes(InputcustomerName)) ||
+            (sale.Craft_fertilizerName && sale.Craft_fertilizerName.includes(InputcustomerName)));
+
+        return InputcustomerID && InputcustomerName
+          ? inputCustomerIDMatch && inputCustomerNameMatch
+          : InputcustomerID
+            ? inputCustomerIDMatch
+            : InputcustomerName
+              ? inputCustomerNameMatch
+              : true;
+      });
+
+      setFilteredSalesData(filteredData);
+    }
+  };
+  const displayData = searchButtonClicked ? filteredSalesData : posts;
+  useEffect(() => {
+    filterSalesData();
+  }, [searchButtonClicked]);
+
+  //เพิ่มสินค้าใหม่
+  const [isDropdownAdd, setIsDropdownAdd] = useState(false);
+  const toggleDropdownAdd = () => {
+    setIsDropdownAdd((prev) => !prev);
   };
 
+  //resat button
+  const handleReset = () => {
+    // Reset input values
+    setInputcustomerID('');
+    setInputcustomerName('');
+
+    // Clear the filtered data
+    setFilteredSalesData([]);
+
+    // Set searchButtonClicked to false to display all data
+    setSearchButtonClicked(false);
+  };
 
   return (
-    <div className='mb-16'>
-      <form onSubmit={onSubmit} method="post">
-        <div>
-          {image ? (
-            <img src={URL.createObjectURL(image)} alt="Uploaded Image" />
-          ) : (
-            <img src="/logo.jpg" alt="logo" />
+    <>
+      <head>
+        <title>Customer</title>
+      </head>
+      <body>
+        <div className="" style={{ width: '100%' }}>
 
-          )}
-          <label htmlFor="sdfsf">บันทุึกรูปภาพ: </label>
-          <input type="file" name="FertilizerPhoto" accept="image/*" onChange={handleFileChange} /><br />
+          <div class="m-4 border-solid border-2 rounded">
+            <div class="p-4 bg-[#777777] font-bold text-white">
+              ค้นหาข้อมูล
+            </div>
+
+            <div class="p-4">
+              <div class="flex -mx-2">
+                <div class="w-1/2 px-2">
+                  <div class="h-12">
+                    <div class="mb-4">
+                      <form action="" method="get">
+                        <label for="customerID">รหัสสินค้า</label>
+                        <input
+                          value={InputcustomerID}
+                          onChange={(e) => setInputcustomerID(e.target.value)}
+                          type="text" list="customerID"
+                          class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" placeholder="กรอกรหัสสินค้า" />
+                        <datalist id="customerID">
+                          {customerIdOptions}
+                        </datalist>
+                      </form>
+                    </div>
+                  </div>
+                </div>
+                <div class="w-1/2 px-2">
+                  <div class="h-12">
+                    <div class="mb-4">
+                      <form action="" method="get">
+                        <label for="customerName">ชื่อสินค้า</label>
+                        <input
+                          value={InputcustomerName}
+                          onChange={(e) => setInputcustomerName(e.target.value)}
+                          type="text" list="customerName"
+                          class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" placeholder="กรอกชื่อสินค้า" />
+                        <datalist id="customerName">
+                          {customerNameOptions}
+                        </datalist>
+                      </form>
+
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="p-4 flex flex-row-reverse">
+              <div class="ps-4">
+                <button onClick={handleReset}
+                  class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded">
+                  รีเซต
+                </button>
+              </div>
+              <div class="ps-4">
+                <button
+                  onClick={() => {
+                    setSearchButtonClicked(true);
+                    filterSalesData();
+                  }}
+                  class="bg-[#00A84F] hover:bg-[#008B41] text-white font-bold py-2 px-4 rounded inline-flex items-center">
+                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="h-5 w-5">
+                    <path fill-rule="evenodd" d="M9 3.5a5.5 5.5 0 100 11 5.5 5.5 0 000-11zM2 9a7 7 0 1112.452 4.391l3.328 3.329a.75.75 0 11-1.06 1.06l-3.329-3.328A7 7 0 012 9z" clip-rule="evenodd" /></svg>
+                  <span>ค้นหา</span>
+                </button>
+              </div>
+            </div>
+          </div>
+
+          <div class="font-bold mx-4 mt-8">สินค้าทั้งหมด</div>
+          <div class="mx-4 mt-2">Stock : <span className="text-[#00A84F]">0</span> ชิ้น</div>
+
+          <div class="flex justify-between mx-4 mt-4">
+            <div class="inline-block relative w-fit">
+              <select class="block appearance-none w-full rounded-md border border-[#e0e0e0] bg-white py-2 pl-6 pr-8 text-base outline-none focus:border-[#6A64F1] focus:shadow-md">
+                <option disabled selected>เลือกหมวดหมู่</option>
+                <option>1</option>
+                <option>2</option>
+                <option>3</option>
+              </select>
+              <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+                <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" /></svg>
+              </div>
+            </div>
+            <div class="">
+              <div class="flex flex-row-reverse">
+                <div class="ps-4">
+                  <div class="inline-block relative w-fit">
+                    <button onClick={toggleDropdownAdd} class="bg-[#00A84F] hover:bg-[#008B41] text-white font-bold py-2 px-4 rounded inline-flex items-center">
+                      <span className="pr-2">เพิ่มสินค้าใหม่</span>
+                      <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" /></svg>
+                    </button>
+                    {isDropdownAdd && (
+                      <div class="absolute mt-2 right-0 w-40 bg-black bg-gray-100 rounded-md shadow-xl z-10 ">
+                        <a href="/product/add_fertilizer" class="block px-2 py-2 text-sm text-black hover:bg-gray-400 hover:text-white">
+                          <div class="flex items-start p-1">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 p-1">
+                              <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                            </svg>
+                            <span>เพิ่มปุ๋ย</span>
+                          </div>
+                        </a>
+                        <a href="/product/add_craft_fertilizer" class="block px-2 py-2 text-sm text-black hover:bg-gray-400 hover:text-white">
+                          <div class="flex items-start p-1">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 p-1">
+                              <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                            </svg>
+                            <span>เพิ่มปุ๋ยผสม</span>
+                          </div>
+                        </a>
+                        <a href="/product/add_chemical" class="block px-2 py-2 text-sm text-black hover:bg-gray-400 hover:text-white">
+                          <div class="flex items-start p-1">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 p-1">
+                              <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                            </svg>
+                            <span>เพิ่มเคมีภัณฑ์</span>
+                          </div>
+                        </a>
+                        <a href="/product/add_other" class="block px-2 py-2 text-sm text-black hover:bg-gray-400 hover:text-white">
+                          <div class="flex items-start p-1">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 p-1">
+                              <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                            </svg>
+                            <span>เพิ่มสินค้าอื่น ๆ</span>
+                          </div>
+                        </a>
+                        {/* Add more dropdown items as needed */}
+                      </div>
+                    )}
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="flex flex-col m-4">
+            <div class="overflow-x-auto sm:-mx-6 lg:-mx-8">
+              <div class="inline-block min-w-full py-2 sm:px-6 lg:px-8">
+                <div class="overflow-hidden">
+                  <Posts posts={displayData} />
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-
-        <label htmlFor="sdfsf">รหัสสินค้า: </label>
-        <input type="text" name="Item_ItemId" value={ItemId} readOnly /><br /><br />
-
-        <label htmlFor="sdfsf">ชื่อปุ๋ย: </label>
-        <input type="text" name="FertilizerName" /><br /><br />
-
-        <label htmlFor="sdfsf">ชื่อสูตรปุ๋ย: </label>
-        <input type="text" name="FertilizerFormulaName" /><br /><br />
-
-        <label htmlFor="sdfsf">ประเภทของปุ๋ย: </label>
-        <input type="text" name="FertilizerType" /><br /><br />
-
-        <label htmlFor="sdfsf">ราคาขาย: </label>
-        <input type="number" name="FertilizerPrice" min={0} /><br /><br />
-
-        <label htmlFor="sdfsf">หน่วยนับ: </label>
-        <select name="ItemUnitId">
-          {ItemUnitId.map(unit => (
-            <option key={unit.UnitId} value={unit.UnitId}>
-              {unit.UnitName}
-            </option>
-          ))}
-        </select><br /><br />
-
-        <label htmlFor="sdfsf">ปริมาณ: </label>
-        <input type="number" name="FertilizerWeigth" min={0} />
-
-        <select className="ml-2" name="FertilizerUnitId">
-          {fertilizerUnitId.map(unit => (
-            <option key={unit.UnitId} value={unit.UnitId}>
-              {unit.UnitName}
-            </option>
-          ))}
-        </select><br /><br />
-
-        <button type="submit">Submit</button>
-      </form>
-    </div>
+      </body>
+    </>
   );
 }
